9c3cbdd4455cf460cdf21b9f299b6d8d6e2b50b7
xianzixiang <xianzixiang@gmail.com>
2019-02-18 10:26:41 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 cd `dirname $0`
-sudo docker run -d -p 8080:8080 --rm -ti --name php-nginx xianzixiang/php-nginx
+sudo docker run -d -p 8080:8080 -v /var/www/blog:/var/www/html --rm -ti --name php-nginx xianzixiang/php-nginx
