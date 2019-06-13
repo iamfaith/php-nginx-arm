59cb9cfd155c299684a514bb7cbfdad652857d35
xianzixiang <xianzixiang@gmail.com>
2019-06-13 14:58:01 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 cd `dirname $0`
-sudo docker build -t xianzixiang/php-nginx .
+sudo docker build -t xianzixiang/php-nginx-arm .
 sudo docker rm $(docker ps -aq)
 sudo docker rmi $(sudo docker images -a| grep none | awk '{print $3}')
